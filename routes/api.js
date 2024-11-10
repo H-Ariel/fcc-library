71d565a8c4694d93021518e28f2e2ad5836d6ede
ariel halili <arielhalili259@gmail.com>
2024-11-10 15:14:03 +0200

    upload project

--- routes/api.js
+++ routes/api.js
@@ -8,40 +8,73 @@
 
 'use strict';
 
+require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  comments: [String]
+});
+const Book = mongoose.model('Book', bookSchema);
+
 module.exports = function (app) {
-
   app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    .get(function (req, res) {
+      const { _id } = req.query;
+      let book = _id == undefined ? Book.find({}) : Book.find({ _id });
+      book.then(data => res.json(data.map(obj => ({
+        _id: obj._id,
+        title: obj.title,
+        comments: obj.comments,
+        commentcount: obj.comments.length
+      })))
+      ).catch(err => res.json({ error: err.message }));
     })
-    
-    .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
+    .post(function (req, res) {
+      const title = req.body.title;
+      if (title === undefined)
+        res.json('missing required field title');
+      else
+        new Book({ title })
+          .save()
+          .then(data => res.json(data))
+          .catch(err => res.json({ error: err.message }));
     })
-    
-    .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+    .delete(function (req, res) {
+      Book.deleteMany({})
+        .then(() => res.json('complete delete successful'))
+        .catch(err => res.json({ error: err.message }));
     });
-
-
 
   app.route('/api/books/:id')
-    .get(function (req, res){
-      let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+    .get(function (req, res) {
+      Book.findOne({ _id: req.params.id })
+        .then(data => res.json(data ? data : 'no book exists'))
+        .catch(err => res.json('no book exists'));
     })
-    
-    .post(function(req, res){
-      let bookid = req.params.id;
-      let comment = req.body.comment;
-      //json res format same as .get
+    .post(function (req, res) {
+      const comment = req.body.comment;
+      if (comment === undefined)
+        return res.json('missing required field comment');
+
+      Book.findOne({ _id: req.params.id })
+        .then(book => {
+          if (book) {
+            book.comments.push(comment);
+            book.save()
+              .then(data => res.json(data))
+              .catch(err => res.json({ error: err.message }));
+          }
+          else {
+            res.json('no book exists');
+          }
+        })
+        .catch(err => res.json('no book exists'));
     })
-    
-    .delete(function(req, res){
-      let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+    .delete(function (req, res) {
+      Book.deleteOne({ _id: req.params.id })
+        .then(data => res.json(data.deletedCount ? 'delete successful' : 'no book exists'))
+        .catch(err => res.json('no book exists'));
     });
-  
 };
